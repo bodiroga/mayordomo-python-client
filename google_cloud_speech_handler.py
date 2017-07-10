c31f0b35df14e646f47ee8a6e725f264c78c1616
Aitor <riturrioz@gmail.com>
2017-07-10 19:46:47 +0200

    Update google_cloud_speech_handler to use the final version of the API

--- google_cloud_speech_handler.py
+++ google_cloud_speech_handler.py
@@ -1,34 +1,27 @@
-#!/usr/bin/python
-# Copyright (C) 2016 Google Inc.
+#!/usr/bin/env python
+
+# Copyright 2017 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Sample that streams audio to the Google Cloud Speech API via GRPC."""
+
 
 from __future__ import division
 
-import contextlib
-import functools
+import os
 import re
 import sys
-import os
 
-
-import google.auth
-import google.auth.transport.grpc
-import google.auth.transport.requests
-from google.cloud.grpc.speech.v1beta1 import cloud_speech_pb2
-from google.rpc import code_pb2
-import grpc
+from google.cloud import speech
 import pyaudio
 from six.moves import queue
 
@@ -37,150 +30,84 @@ LANGUAGE = "en-US"
 RATE = 16000
 CHUNK = int(RATE / 10)  # 100ms
 
-# The Speech API has a streaming limit of 60 seconds of audio*, so keep the
-# connection alive for that long, plus some more to give the API time to figure
-# out the transcription.
-# * https://g.co/cloud/speech/limits#content
-DEADLINE_SECS = 60 * 3 + 5
-SPEECH_SCOPE = 'https://www.googleapis.com/auth/cloud-platform'
 
+class MicAsFile(object):
+    """Opens a recording stream as a file-like object."""
+    def __init__(self, rate, chunk):
+        self._rate = rate
+        self._chunk = chunk
 
-def make_channel(host, port):
-    """Creates a secure channel with auth credentials from the environment."""
-    # Grab application default credentials from the environment
-    credentials, _ = google.auth.default(scopes=[SPEECH_SCOPE])
+        # Create a thread-safe buffer of audio data
+        self._buff = queue.Queue()
+        self.closed = True
 
-    # Create a secure channel using the credentials.
-    http_request = google.auth.transport.requests.Request()
-    target = '{}:{}'.format(host, port)
+    def __enter__(self):
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            # The API currently only supports 1-channel (mono) audio
+            # https://goo.gl/z757pE
+            channels=1, rate=self._rate,
+            input=True, frames_per_buffer=self._chunk,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
+            stream_callback=self._fill_buffer,
+        )
 
-    return google.auth.transport.grpc.secure_authorized_channel(
-        credentials, http_request, target)
+        self.closed = False
 
+        return self
 
-def _audio_data_generator(buff):
-    """A generator that yields all available data in the given buffer.
+    def __exit__(self, type, value, traceback):
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        # Flush out the read, just in case
+        self._buff.put(None)
+        self._audio_interface.terminate()
 
-    Args:
-        buff - a Queue object, where each element is a chunk of data.
-    Yields:
-        A chunk of data that is the aggregate of all chunks of data in `buff`.
-        The function will block until at least one data chunk is available.
-    """
-    stop = False
-    while not stop:
+    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
+        """Continuously collect data from the audio stream, into the buffer."""
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def read(self, chunk_size):
+        if self.closed:
+            return
+
         # Use a blocking get() to ensure there's at least one chunk of data.
-        data = [buff.get()]
+        data = [self._buff.get()]
 
         # Now consume whatever other data's still buffered.
         while True:
             try:
-                data.append(buff.get(block=False))
+                data.append(self._buff.get(block=False))
             except queue.Empty:
                 break
 
-        # `None` in the buffer signals that the audio stream is closed. Yield
-        # the final bit of the buffer and exit the loop.
-        if None in data:
-            stop = True
-            data.remove(None)
-
-        yield b''.join(data)
-
-
-def _fill_buffer(buff, in_data, frame_count, time_info, status_flags):
-    """Continuously collect data from the audio stream, into the buffer."""
-    buff.put(in_data)
-    return None, pyaudio.paContinue
-
-
-# [START audio_stream]
-@contextlib.contextmanager
-def record_audio(rate, chunk):
-    """Opens a recording stream in a context manager."""
-    # Create a thread-safe buffer of audio data
-    buff = queue.Queue()
-
-    audio_interface = pyaudio.PyAudio()
-    audio_stream = audio_interface.open(
-        format=pyaudio.paInt16,
-        # The API currently only supports 1-channel (mono) audio
-        # https://goo.gl/z757pE
-        channels=1, rate=rate,
-        input=True, frames_per_buffer=chunk,
-        # Run the audio stream asynchronously to fill the buffer object.
-        # This is necessary so that the input device's buffer doesn't overflow
-        # while the calling thread makes network requests, etc.
-        stream_callback=functools.partial(_fill_buffer, buff),
-    )
-
-    yield _audio_data_generator(buff)
-
-    audio_stream.stop_stream()
-    audio_stream.close()
-    # Signal the _audio_data_generator to finish
-    buff.put(None)
-    audio_interface.terminate()
+        if self.closed:
+            return
+        return b''.join(data)
 # [END audio_stream]
 
 
-def request_stream(data_stream, rate, single_utterance=True, interim_results=True):
-    """Yields `StreamingRecognizeRequest`s constructed from a recording audio
-    stream.
-
-    Args:
-        data_stream: A generator that yields raw audio data to send.
-        rate: The sampling rate in hertz.
-        interim_results: Whether to return intermediate results, before the
-            transcription is finalized.
-    """
-    # The initial request must contain metadata about the stream, so the
-    # server knows how to interpret it.
-    recognition_config = cloud_speech_pb2.RecognitionConfig(
-        # There are a bunch of config options you can specify. See
-        # https://goo.gl/KPZn97 for the full list.
-        encoding='LINEAR16',  # raw 16-bit signed LE samples
-        sample_rate=rate,  # the rate in hertz
-        # See http://g.co/cloud/speech/docs/languages
-        # for a list of supported languages.
-        language_code=LANGUAGE,  # a BCP-47 language tag
-    )
-    streaming_config = cloud_speech_pb2.StreamingRecognitionConfig(
-        interim_results=interim_results,
-        single_utterance=single_utterance,
-        config=recognition_config,
-    )
-
-    yield cloud_speech_pb2.StreamingRecognizeRequest(
-        streaming_config=streaming_config)
-
-    for data in data_stream:
-        # Subsequent requests can all just have the content
-        yield cloud_speech_pb2.StreamingRecognizeRequest(audio_content=data)
-
-
-def listen_print_loop(recognize_stream):
+def listen_print_loop(results_gen):
     """Iterates through server responses and prints them.
-
-    The recognize_stream passed is a generator that will block until a response
+    The results_gen passed is a generator that will block until a response
     is provided by the server. When the transcription response comes, print it.
-
     In this case, responses are provided for interim results as well. If the
     response is an interim one, print a line feed at the end of it, to allow
     the next result to overwrite it, until the response is a final one. For the
     final one, print a newline to preserve the finalized transcription.
     """
     num_chars_printed = 0
-    for resp in recognize_stream:
-        if resp.error.code != code_pb2.OK:
-            raise RuntimeError('Server error: ' + resp.error.message)
-
-        if not resp.results:
+    for result in results_gen:
+        if not result.alternatives:
             continue
 
         # Display the top transcription
-        result = resp.results[0]
-        transcript = result.alternatives[0].transcript
+        transcript = result.transcript
 
         # Display interim results, but with a carriage return at the end of the
         # line, so subsequent lines will overwrite them.
@@ -192,42 +119,37 @@ def listen_print_loop(recognize_stream):
         if not result.is_final:
             sys.stdout.write(transcript + overwrite_chars + '\r')
             sys.stdout.flush()
+
             num_chars_printed = len(transcript)
+
             if re.search(r'\b(por favor)\b', transcript, re.I):
                 return(transcript)
 
         else:
-            return(transcript)
+            print(transcript + overwrite_chars)
+
+            return (transcript)
 
             num_chars_printed = 0
 
 
 def start():
-    with record_audio(RATE, CHUNK) as buffered_audio_data:
-        service = cloud_speech_pb2.SpeechStub(
-            make_channel('speech.googleapis.com', 443))
+    speech_client = speech.Client()
 
-        requests = request_stream(buffered_audio_data, RATE)
-
-        recognize_stream = service.StreamingRecognize(
-            requests, DEADLINE_SECS)
-
+    with MicAsFile(RATE, CHUNK) as stream:
+        audio_sample = speech_client.sample(
+            stream=stream,
+            encoding=speech.encoding.Encoding.LINEAR16,
+            sample_rate_hertz=RATE)
+        # See http://g.co/cloud/speech/docs/languages
+        # for a list of supported languages.
+        language_code = LANGUAGE  # a BCP-47 language tag
+        results_gen = audio_sample.streaming_recognize(
+                language_code=language_code, interim_results=True)
 
         # Now, put the transcription responses to use.
-        try:
-            result = listen_print_loop(recognize_stream)
-            recognize_stream.cancel()
-            return result
-        except grpc.RpcError as e:
-            code = e.code()
-            # CANCELLED is caused by the interrupt handler, which is expected.
-            if code is not code.CANCELLED:
-                raise
-        except RuntimeError as e:
-            if "deadline too short" in e.message:
-                return None
-            else:
-                raise
+        result = listen_print_loop(results_gen)
+        return result
 
 
 def stop():
